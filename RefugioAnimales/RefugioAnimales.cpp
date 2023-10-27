76aefd852f1b58c9424385526e27c5b2070834af
Ezequiel de la Fuente <feracosentino@gmail.com>
2023-10-27 09:34:46 -0300

    base

--- RefugioAnimales/RefugioAnimales.cpp
+++ RefugioAnimales/RefugioAnimales.cpp
@@ -1,8 +1,21 @@
 #include "pch.h"
+#include "MyForm.h"
+#include <windows.h>
+
 
 using namespace System;
+using namespace System::Windows::Forms;
 
-int main(array<System::String ^> ^args)
-{
+
+int main(array<System::String^>^ args) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    // Crea una instancia del formulario
+    RefugioAnimales::MyForm^ form = gcnew RefugioAnimales::MyForm();
+
+    // Ejecuta la aplicación
+    Application::Run(form);
+
     return 0;
 }
